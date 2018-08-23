4a0f0dfe79c577e78fd216f03bafe516d04b97c4
caiothizio <caiothizio@alunos.utfpr.edu.br>
2018-08-23 20:14:26 -0300

    Add Mediana

---
+++ Atividade06/src/main/java/br/edu/utfpr/atividade06/Mediana.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.atividade06;
+
+/**
+ *
+ * @author caiot
+ */
+public class Mediana {
+    public double getMediana(double[] vet){
+        return 3;
+    }
+}
---
+++ Atividade06/src/test/java/br/edu/utfpr/atividade06/MedianaTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.utfpr.atividade06;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author caiot
+ */
+public class MedianaTest {
+    
+    public MedianaTest() {
+    }
+    
+    @Test
+    public void testeMedianaVetorOrdenadoImpar(){
+        double[] vet = {1, 2, 3, 4, 5};
+        assertEquals(3f, new Mediana().getMediana(vet), 0.01);
+    }
+}
