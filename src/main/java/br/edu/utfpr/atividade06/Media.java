cf94b4b1369eef735b5b2a6365a2126c21a894ed
caiothizio <caiothizio@alunos.utfpr.edu.br>
2018-08-25 15:53:03 -0300

    Fazendo teste passar (add classe Media)

---
+++ src/main/java/br/edu/utfpr/atividade06/Media.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.atividade06;
+
+/**
+ *
+ * @author caiot
+ */
+class Media {
+
+    double media(double[] vet) {
+        return 3.0;
+    }
+
+    double menor(double[] vet) {
+        return 1.0;
+    }
+
+    double maior(double[] vet) {
+        return 5.0;
+    }
+
+    Object qtdMaiores(double[] vet) {
+        return 2;
+    }
+
+    Object qtdMenores(double[] vet) {
+        return 2;
+    }
+    
+}
