5b59338449d7e0a9886fd06e068297b6f4b82426
caiothizio <caiothizio@alunos.utfpr.edu.br>
2018-08-25 17:20:46 -0300

    Fazendo teste de n Maiores e n Menores passar

---
+++ src/main/java/br/edu/utfpr/atividade06/NMaioresEMenores.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.atividade06;
+
+/**
+ *
+ * @author caiot
+ */
+class NMaioresEMenores {
+
+    double[] getNMaiores(double[] vet, int i) {
+        double v[] = {4, 5};
+        return v;
+    }
+
+    double[] getNMenores(double[] vet, int i) {
+        double v[] = {1};
+        return v;
+    }
+    
+}
